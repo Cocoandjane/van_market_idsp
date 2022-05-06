c107f02ce3b296c7a844d060b7f6b7ee1c872b6c
Feiyang <feiyang@Feiyangs-MacBook-Pro.local>
2022-05-06 13:31:03 -0700

    heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //const app = require("./app.js")
 import app from "./app.js"
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`server should be running at http://localhost:${port}/`))
+
