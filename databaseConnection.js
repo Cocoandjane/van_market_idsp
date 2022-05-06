98454057073847b4521a3293476c47a38784a46a
Murad Qumizakis <muradqumizakis@gmail.com>
2022-05-06 16:25:24 -0700

    resolved

--- databaseConnection.js
+++ databaseConnection.js
@@ -4,19 +4,11 @@ import mysql from 'mysql2'
 const is_heroku = process.env.IS_HEROKU || false;
 
 const dbConfigHeroku = {
-<<<<<<< HEAD
-	host: "acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-	user: "i40wodotvago9jjp",
-	password: "fwde7xuqb907y8k2",
-	database: "su8ad2o8zsnbmrtn",
-	multipleStatements: false,
-=======
 	host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 	user: "k23o5dcq3pwfm61i",
 	password: "t3g6tvncln9peoxz",
 	database: "hvake130agwnhd4q",
 	multipleStatements: true,
->>>>>>> c107f02ce3b296c7a844d060b7f6b7ee1c872b6c
 	namedPlaceholders: true
 };
 
