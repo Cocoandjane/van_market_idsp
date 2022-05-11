a70cf0a7c7398599cc3ad6dc0e53526fc71d3ddb
Feiyang <feiyang@Feiyangs-MacBook-Pro.local>
2022-05-11 12:42:16 -0700

    event fixed

--- static/viewListing.js
+++ static/viewListing.js
@@ -4,21 +4,19 @@ let deleteBtn = document.querySelector("a.delete")
 deleteBtn.addEventListener("click", (event) => {
     let card = document.querySelector(".card")
     card.classList.remove("displayNone");
-
-    let no = document.querySelector("button.no");
-    no.addEventListener("click", (event) => {
-        let card = document.querySelector(".card")
-        card.classList.add("displayNone")
-        event.preventDefault();
-    })
-
-    let yes = document.querySelector("button.yes");
-    yes.addEventListener("click", (event) => {
-        let x = yes.action
-        window.alert("product deleted")
-        window.location = x
-        event.stopPropagation();
-    })
-    event.preventDefault();
 })
 
+let yes = document.querySelector("button.yes");
+yes.addEventListener("click", (event) => {
+    let x = yes.action
+    window.alert("product deleted")
+    window.location = x
+    event.stopPropagation();
+})
+
+let no = document.querySelector("button.no");
+no.addEventListener("click", (event) => {
+    let card = document.querySelector(".card")
+    card.classList.add("displayNone")
+    event.preventDefault();
+})
