dbaf3dc3b8740f73b7879085897e42c95467b7e3
Feiyang <feiyang@Feiyangs-MacBook-Pro.local>
2022-05-19 17:07:08 -0700

    heroku

--- static/chat.js
+++ static/chat.js
@@ -3,11 +3,11 @@ const chatForm = document.getElementById('chat-form')
 const chatMessages = document.querySelector('.chat-messages')
 const socket = io();
 
-axios.get("/curentUserName")
-.then(response => {
-    //console.log(response.data.username)
-    let curUser = response.data.username
-    console.log(curUser)
+// axios.get("/curentUserName")
+// .then(response => {
+//     //console.log(response.data.username)
+//     let curUser = response.data.username
+//     console.log(curUser)
 
 
 // console.log(curUser)
@@ -54,4 +54,4 @@ chatForm.addEventListener('submit', (e) => {
     e.target.elements.msg.value = ''
     e.target.elements.msg.focus()
 })
-})
+// })
