257e3a41eaef41b5a9d049667cf289538590d460
Feiyang <feiyang@Feiyangs-MacBook-Pro.local>
2022-05-22 11:33:45 -0700

    he

--- static/frontend.js
+++ static/frontend.js
@@ -1,47 +1,6 @@
 
 
 
-let addNewPost = document.querySelector(".postNew")
-addNewPost.addEventListener("click", function (event) {
-    let catagorySlide = document.querySelector("div.createNew.displayNone")
-    catagorySlide.classList.remove("displayNone")
-    event.preventDefault()
-})
-
-let main = document.querySelector("main")
-// console.log(main)
-// main.addEventListener("click", function (event) {
-//     let catagorySlide = document.querySelector("div.createNew")
-//     catagorySlide.classList.add("displayNone")
-//     event.preventDefault()
-// })
-
-let addPostElectronics = document.querySelector('.catagories.electronics')
-addPostElectronics.addEventListener('click', () => {
-    location.href = '/createListing'
-})
-
-let addPostClothes = document.querySelector('.catagories.clothes')
-addPostClothes.addEventListener('click', () => {
-    location.href = '/createListing'
-})
-
-let addPostFurniture = document.querySelector('.catagories.furniture')
-addPostFurniture.addEventListener('click', () => {
-    location.href = '/createListing'
-})
-
-
-let addPostOther = document.querySelector('.catagories.other')
-addPostOther.addEventListener('click', () => {
-    location.href = '/createListing'
-})
-
-let bar = document.querySelector(".bar")
-bar.addEventListener("click", (event)=>{
-    let catagorySlide = document.querySelector("div.createNew")
-    catagorySlide.classList.add("displayNone")
-})
 
 // let viewProductButton = document.querySelector(`.card`); // change this to be the .viewProductButton
 // viewProductButton.addEventListener('click', (e) => {
