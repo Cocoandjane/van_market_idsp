c2a475930b3b657081288d977b6896192db716b7
Feiyang <feiyang@Feiyangs-MacBook-Pro.local>
2022-05-27 09:43:08 -0700

    a

--- static/chatList.js
+++ static/chatList.js
@@ -1,36 +1,40 @@
-
-
-let chats = document.querySelectorAll(".chatPerson")
-
-let recentMsg = document.querySelectorAll(".recentMsg")
-recentMsg.forEach(n => {
-    let roomIds = n.dataset.id
-    for (let roomId of roomIds) {
-        let chatHistery = JSON.parse(localStorage.getItem(roomId))
-        n.innerHTML = chatHistery[chatHistery.length - 1].text
-    }
-})
-
-
-let recentTime = document.querySelectorAll(".time")
-recentTime.forEach(n => {
-    let roomIds = n.dataset.id
-    for (let roomId of roomIds) {
-        let chatHistery = JSON.parse(localStorage.getItem(roomId))
-        n.innerHTML = chatHistery[chatHistery.length - 1].time
-    }
-})
-// recentTime.innerHTML = chatHistery[chatHistery.length - 1].time
-
 let people = document.querySelectorAll(".all")
 people.forEach(p => {
-    let roomId = p.dataset.id
     p.addEventListener("click", (event) => {
+        console.log("click")
         event.preventDefault()
-        // let id =chat.dataset.id
-        window.location = `/chat/${roomId}`
+        let id =p.dataset.id
+        window.location = `/chat/${id}`
     })
 })
 
+let recentMsg = document.querySelectorAll(".recentMsg")
+for (let i = 0; i < recentMsg.length; i++) {
+let roomIds = recentMsg[i].dataset.id
+for (let roomId of roomIds) {
+let chatHistery = JSON.parse(localStorage.getItem(roomId))
+recentMsg[i].innerHTML = chatHistery[recentMsg.length-1].text
+}
+}
 
-// console.log(localStorage.getItem(1))
+let recentTime = document.querySelectorAll(".time")
+for (let i = 0; i < recentTime.length; i++) {
+    let roomIds = recentTime[i].dataset.id
+    for (let roomId of roomIds) {
+    let chatHistery = JSON.parse(localStorage.getItem(roomId))
+    recentTime[i].innerHTML = chatHistery[recentTime.length-1].time
+    }
+}
+
+
+
+// let recentTime = document.querySelectorAll(".time")
+// recentTime.forEach(n => {
+//     let roomIds = n.dataset.id
+//     for (let roomId of roomIds) {
+//         let chatHistery = JSON.parse(localStorage.getItem(roomId))
+//         n.innerHTML = chatHistery[chatHistery.length - 1].time
+//     }    
+// })    
+
+
