518b2cf40c9ecc533aa2840a57976bfdae627f46
Feiyang <feiyang@Feiyangs-MacBook-Pro.local>
2022-05-31 00:20:06 -0700

    a

--- static/editPost.js
+++ static/editPost.js
@@ -51,7 +51,7 @@ imageForm.addEventListener("submit", async event => {
             let id = response.data
             //location.href = `/viewListing`
             // success go to the next page
-            // window.location = `/viewListing/${id}`
+            window.location = `/viewListing/${id}`
         })
         .catch((err) => {
             console.log('ERR', err)
